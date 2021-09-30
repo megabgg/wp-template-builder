6cccd214b522a0b7805e28c93122f0c5793cdaf6
Andrey Gorshkov <megabgg@yandex.ru>
2021-09-30 11:54:06 +0300

    disable assets for WTB_MODE = NO_VIEW

--- includes/assets.php
+++ includes/assets.php
@@ -4,7 +4,9 @@
  * Adding assets
  */
 add_action('wp_enqueue_scripts', function () {
-    $ver = 0001;
-    wp_enqueue_style('style', get_template_directory_uri() . '/resources/static/css/main.css', array(), $ver, 'all');
-    wp_enqueue_script('ui', get_template_directory_uri() . '/resources/static/js/main.js', array(), $ver, false);
+    if (!defined('WTB_MODE') || (defined('WTB_MODE') && constant("WTB_MODE") != 'NO_VIEW')) {
+        $ver = 0001;
+        wp_enqueue_style('style', get_template_directory_uri() . '/resources/static/css/main.css', array(), $ver, 'all');
+        wp_enqueue_script('ui', get_template_directory_uri() . '/resources/static/js/main.js', array(), $ver, false);
+    }
 });
