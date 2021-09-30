90b4c8baa7b52c2b4fa6a6091e7547bdf0984565
Andrey Gorshkov <megabgg@yandex.ru>
2021-09-30 20:29:09 +0300

    Added init function in acf-map module

--- includes/modules/acf-map/index.php
+++ includes/modules/acf-map/index.php
@@ -7,7 +7,9 @@ add_action('acf/init', function () {
     acf_update_setting('google_api_key', API_GOOGLE_KEY);
 });
 
-wp_enqueue_script('google_api', "https://maps.googleapis.com/maps/api/js?key=" . API_GOOGLE_KEY, array());
-wp_enqueue_script('map_js', get_template_directory_uri() . '/includes/modules/acf-map/js/map.js', array(), $ver, false);
-wp_enqueue_style('map_style', get_template_directory_uri() . '/includes/modules/acf-map/css/style.css', array(), $ver, 'all');
-
+function init_acf_map()
+{
+    wp_enqueue_script('google_api', "https://maps.googleapis.com/maps/api/js?key=" . API_GOOGLE_KEY, array());
+    wp_enqueue_script('map_js', get_template_directory_uri() . '/includes/modules/acf-map/js/map.js', array(), $ver, false);
+    wp_enqueue_style('map_style', get_template_directory_uri() . '/includes/modules/acf-map/css/style.css', array(), $ver, 'all');
+}
