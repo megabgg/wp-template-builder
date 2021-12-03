067b87a3a19f1207bc8ee97f8f5c7edf53c5f98c
Andrey Gorshkov <megabgg@yandex.ru>
2021-12-03 17:20:54 +0300

    Объединил области меню в один обработчик

--- includes/menus.php
+++ includes/menus.php
@@ -7,9 +7,6 @@ add_action('after_setup_theme', function () {
     register_nav_menus(array(
         'menu-top' => __('Top menu', 'wp-template-builder'),
     ));
-});
-
-add_action('after_setup_theme', function () {
     register_nav_menus(array(
         'menu-footer' => __('Footer menu', 'wp-template-builder'),
     ));
